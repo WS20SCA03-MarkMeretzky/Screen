48dd816b69782deb916bb977a3dc4bdb3b810c57
Mark Meretzky <mark.meretzky@gmail.com>
2020-03-28 14:39:07 -0400

    Initial Commit

---
+++ Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Screen
+//
+//  Created by Mark Meretzky on 3/28/20.
+//  Copyright © 2020 New York University School of Professional Studies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
